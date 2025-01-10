f1e79c1042a29a55602219b9e8850297c3eb0ba4
sigmel <sigmel@gmail.com>
2025-01-09 22:56:49 -0600

    Code cleanup on D3D12Buffer to use designated initialization

--- Engine/Source/Private/Render/DirectX12/DirectX12Buffer.cpp
+++ Engine/Source/Private/Render/DirectX12/DirectX12Buffer.cpp
@@ -8,19 +8,23 @@ DirectX12VertexBuffer::DirectX12VertexBuffer(IRenderDevice* device, const void* 
 {
 	DirectX12Device* dx12Device = static_cast<DirectX12Device*>(device);
 
-	D3D12_HEAP_PROPERTIES heapProperties = {};
-	heapProperties.Type = D3D12_HEAP_TYPE_DEFAULT;
-	heapProperties.CreationNodeMask = 1;
-	heapProperties.VisibleNodeMask = 1;
+	D3D12_HEAP_PROPERTIES heapProperties = {
+		.Type = D3D12_HEAP_TYPE_DEFAULT,
+		.CreationNodeMask = 1,
+		.VisibleNodeMask = 1,
+	};
 
-	D3D12_RESOURCE_DESC resourceDesc = {};
-	resourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	resourceDesc.Width = dataSize;
-	resourceDesc.Height = 1;
-	resourceDesc.DepthOrArraySize = 1;
-	resourceDesc.MipLevels = 1;
-	resourceDesc.SampleDesc.Count = 1;
-	resourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	D3D12_RESOURCE_DESC resourceDesc = {
+		.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER,
+		.Width = dataSize,
+		.Height = 1,
+		.DepthOrArraySize = 1,
+		.MipLevels = 1,
+		.SampleDesc = {
+			.Count = 1
+		},
+		.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+	};
 
 	dx12Device->GetDevice()->CreateCommittedResource(
 		&heapProperties,
@@ -41,19 +45,21 @@ DirectX12VertexBuffer::DirectX12VertexBuffer(IRenderDevice* device, const void* 
 		IID_PPV_ARGS(&_uploadVertexBuffer)
 	);
 
-	BYTE* uploadData = nullptr;
+	uint8_t* uploadData = nullptr;
 	_uploadVertexBuffer->Map(0, nullptr, reinterpret_cast<void**>(&uploadData));
 	memcpy(uploadData, data, dataSize);
 	_uploadVertexBuffer->Unmap(0, nullptr);
-
 	dx12Device->GetCommandList()->CopyBufferRegion(_vertexBuffer.Get(), 0, _uploadVertexBuffer.Get(), 0, dataSize);
 
-	D3D12_RESOURCE_BARRIER barrier = {};
-	barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-	barrier.Transition.pResource = _vertexBuffer.Get();
-	barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
-	barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER;
-	barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+	D3D12_RESOURCE_BARRIER barrier = {
+		.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+		.Transition = {
+			.pResource = _vertexBuffer.Get(),
+			.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
+			.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST,
+			.StateAfter = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER,
+		},
+	};
 	dx12Device->GetCommandList()->ResourceBarrier(1, &barrier);
 
 	_vertexBufferView.BufferLocation = _vertexBuffer->GetGPUVirtualAddress();
